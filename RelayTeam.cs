aa45c953efed5da322d5c9fffa0f61b8ba3e0bda
Venugopal K <venu.shastri@gmail.com>
2020-08-13 10:47:22 +0530

    Create RelayTeam.cs

---
+++ RelayTeam.cs
@@ -0,0 +1,47 @@
+ //M2->M1->M4->M3
+    public class RelayTeam
+    {
+        public void Memeber1() {
+        for(int i = 0; i < 10; i++)
+            {
+
+            }
+        }
+        public void Memeber2() {
+        for(int i = 0; i < 20; i++) { }
+        
+        }
+        public void Memeber3() {
+            for (int i = 0; i < 15; i++) { }
+        }
+        public void Memeber4() {
+
+            for (int i = 0; i < 25; i++) { }
+        }
+
+    }
+    
+    //Output
+    
+    Memeber1 Ready To Run
+    Memeber2 Ready To Run
+    Memeber3 Ready To Run
+    Memeber4 Ready To Run
+    
+    Press Any Key Start
+    
+    Memeber2 Running and Reached 1 Meter
+    Memeber2 Running and Reached 2 Meter
+    Memeber2 Running and Reached 3 Meter
+    Memeber2 Running and Reach {} Meter
+    Memeber2 Running and Reach {} Meter
+    Memeber2 Running and Reach {} Meter
+    Memeber2 Running and Reach {} Meter
+    Memeber2 Running and Reach {} Meter
+    Memeber2 Running and Reach {} Meter
+    Memeber2 Running and Reach {} Meter
+    Member2 Completed and handover Ballot to next Member
+    
+    Member1
+    
+    
